6af1c50c906fd8fd8dd46b48d2fdba92f0374df2
nycki <nupanick@gmail.com>
2025-12-20 13:58:56 -0800

    proof of concept

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 import http from 'http';
+import sharp from 'sharp';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  res.write('Hello from Bun');
+const background = sharp({ create: {
+  width: 64,
+  height: 32,
+  channels: 4,
+  background: { r: 255, g: 255, b: 255, alpha: 1 },
+}});
+
+const server = http.createServer(async (req, res) => {
+
+  const img = await (background
+    .composite([
+      { input: 'sitelen/kijetesantakalu.png', top: 0, left: 0 },
+      { input: 'sitelen/musi.png', top: 0, left: 16 },
+    ])
+    .png()
+    .toBuffer()
+    .then(data => sharp(data).resize({ width: 64 * 8, kernel: sharp.kernel.nearest }).toBuffer())
+  );
+
+  res.writeHead(200, { 'content-type': 'image/png' });
+  res.write(img);
   res.end();
 });
 
